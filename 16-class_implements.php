2424405362e26cefea83b4a0d3f5fcfda4987561
Tom Hudson <mail@tomnomnom.com>
2012-07-15 14:35:56 +0100

    class_implements

---
+++ 16-class_implements.php
@@ -0,0 +1,19 @@
+<?php
+// class_implements
+
+class Users implements Countable, IteratorAggregate {
+  protected $users = ['tom', 'dick', 'harry'];
+
+  public function count(){
+    return count($this->users);
+  }
+
+  public function getIterator(){
+    return new ArrayIterator($this->users);
+  }
+}
+
+var_dump(class_implements('Users'));
+
+$users = new Users();
+var_dump(class_implements($users));
