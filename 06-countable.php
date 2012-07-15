a3097b43c6edc3a700993836c51251efb7264e5d
Tom Hudson <mail@tomnomnom.com>
2012-07-15 14:04:38 +0100

    Countable interface

---
+++ 06-countable.php
@@ -0,0 +1,14 @@
+<?php
+// Countable interface
+
+class Users implements Countable {
+  protected $users = ['tom', 'dick', 'harry'];
+
+  public function count(){
+    return count($this->users);
+  }
+}
+
+$users = new Users();
+
+var_dump(count($users));
